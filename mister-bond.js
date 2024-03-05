5893f4f0aac4f11c311ad1945e5dfe6204310273
Roy Eyal <roy.eyal@gmail.com>
2024-03-05 12:38:34 +0200

    Create mister-bond.js

---
+++ mister-bond.js
@@ -0,0 +1,36 @@
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function setupSplits() {
+  const targets = gsap.utils.toArray("p.align-center");
+  targets.forEach((target) => {
+    let splitHeading = new SplitText(target, { type: "words,chars" });
+    let chars = splitHeading.chars; //an array of all the divs that wrap each character
+    gsap.fromTo(
+        chars,
+        { 
+          y: 100,
+          opacity: 0,
+          rotationX: 180,
+		  transformOrigin: "0% 50% -50",
+          scale: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          rotationX: 0,
+          stagger: 0.02,
+          scale: 1,
+          duration: 0.2,
+          ease: 'power4.out',
+          scrollTrigger: {
+				trigger: target,
+				//markers: true,
+				start: "top 85%",
+				end: "bottom center",
+				//scrub: true
+			},
+        }
+      );
+  });
+}
+setupSplits();
